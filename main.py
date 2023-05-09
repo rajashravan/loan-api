6c398fb6c6f4675862558ad882e924a0ec36eff0
Raja Shravan <rajashravan@hotmail.com>
2023-05-09 16:12:51 -0400

    Update main.py
    adds loan methods and other

--- main.py
+++ main.py
@@ -29,6 +29,16 @@ class Loan:
         self.amount = amount
         self.annual_interest_rate = annual_interest_rate
         self.loan_term = loan_term
+        self.loan_schedule = []
+
+    def get_loan_metadata(self) -> dict:
+        return {
+            "loan_id": self.loan_id,
+            "user_id": self.user_id,
+            "amount": self.amount,
+            "annual_interest_rate": self.annual_interest_rate,
+            "loan_term": self.loan_term,
+        }
 
     @staticmethod
     def make_payment(principal_remaining, mpr, monthly_payment) -> dict:
@@ -44,7 +54,10 @@ class Loan:
             "current_interest_payment": round(current_interest_payment, 2),
         }
 
-    def get_loan_schedule(self):
+    def get_loan_schedule(self) -> list:
+        if self.loan_schedule:
+            return self.loan_schedule
+
         apr = self.annual_interest_rate / 100
         mpr = apr / 12
         principal_remaining = self.amount
@@ -71,6 +84,28 @@ class Loan:
         self.loan_schedule = payments
         return self.loan_schedule
 
+    def get_loan_summary_for_month(self, month: int) -> Optional[dict]:
+        """
+        Get the loan summary for a given month.
+        :param month: Month to get the loan summary for.
+        :return: Loan summary for a given month as a dictionary. Else None if month does not exist.
+        """
+        loan_schedule = self.get_loan_schedule()
+        try:
+            return loan_schedule[month-1]
+        except IndexError:
+            return None
+
+    def transfer_to_user(self, user_id: uuid.UUID):
+        self.user_id = user_id
+def create_loan_from_loan_creation_request(loan_creation_request: LoanCreationRequest) -> Loan:
+    user_id = uuid.UUID(loan_creation_request.user_id)
+    amount = loan_creation_request.amount
+    annual_interest_rate = loan_creation_request.annual_interest_rate
+    loan_term = loan_creation_request.loan_term
+    loan_id = uuid.uuid4()
+    loan = Loan(loan_id, user_id, amount, annual_interest_rate, loan_term)
+    return loan
 
 
 def validate_loan_creation_request(loan_creation_request: LoanCreationRequest) -> Optional[str]:
